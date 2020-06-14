0c35016f19287d63843ff85fede9b0d847a7f368
Santosh <get.santoshbhandari@gmail.com>
2020-06-14 17:50:05 +0545

    pagingination  with Retrofit

--- NewsApp/app/src/main/java/com/hamro/newsapp/util/Constants.kt
+++ NewsApp/app/src/main/java/com/hamro/newsapp/util/Constants.kt
@@ -5,5 +5,6 @@ class Constants {
         const val API_KEY = "4af39c1b6c7446a7b1c62945783272e8"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }

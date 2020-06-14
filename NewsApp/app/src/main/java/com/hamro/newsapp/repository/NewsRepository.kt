55dc5c10b9cde537589f771281bc2a8abdf2d2f2
Santosh <get.santoshbhandari@gmail.com>
2020-06-14 16:50:20 +0545

    Adding and deleting Article in Room Databse

--- NewsApp/app/src/main/java/com/hamro/newsapp/repository/NewsRepository.kt
+++ NewsApp/app/src/main/java/com/hamro/newsapp/repository/NewsRepository.kt
@@ -2,9 +2,20 @@ package com.hamro.newsapp.repository
 
 import com.hamro.newsapp.api.RetrofitInstance
 import com.hamro.newsapp.db.ArticleDatabase
+import com.hamro.newsapp.response.Article
 
 class NewsRepository (
     val db: ArticleDatabase){
     suspend fun getBreakingNews(countryCode: String, pageNumber: Int) =
         RetrofitInstance.api.getBreakingNews(countryCode, pageNumber)
+
+    suspend fun searchNews(searchQuery: String, pageNumber: Int) =
+        RetrofitInstance.api.searchForNews(searchQuery, pageNumber)
+
+    suspend fun upsert(article: Article) = db.getArticleDao().upsert(article)
+
+    fun getSavedNews() = db.getArticleDao().getAllArticles()
+
+    suspend fun deleteArticle(article: Article) = db.getArticleDao().deleteArticle(article)
+
 }
--- NewsApp/app/src/main/java/com/hamro/newsapp/util/Constants.kt
+++ NewsApp/app/src/main/java/com/hamro/newsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "4af39c1b6c7446a7b1c62945783272e8"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
